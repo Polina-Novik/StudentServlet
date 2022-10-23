2fcc5c3d39aec295d472a305ab51f9c330a595e0
Polina-Novik <polinochka_novik_2002@mail.ru>
2022-10-23 21:32:21 +0300

    Added Student Servlet

---
+++ src/main/java/com/novik/DatabaseAccess.java
@@ -0,0 +1,53 @@
+package com.novik;
+
+// Loading required libraries
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+
+public class DatabaseAccess extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        final String URL = "jdbc:mysql://localhost:3306/test_db1";
+        final String USERNAME = "root";
+        final String PASSWORD = "Stalker261986";
+        final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+        final String SELECT_ALL_USERS1_QUERY = "select * from users1";
+        final String SELECT_ALL_COUNTER_QUERY = "select * from counter";
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            Statement statement = connection.createStatement();
+            statement.execute("update counter set count=count+1 where id=1");
+            ResultSet resultSet = statement.executeQuery(SELECT_ALL_USERS1_QUERY);
+            while (resultSet.next()) {
+                writer.println("<h1> " + resultSet.getInt("id") + ", name: "
+                        + resultSet.getString("name") + ", course: "
+                        + resultSet.getInt("course") + "</h1>");
+            }
+            ResultSet resultSet1 = statement.executeQuery(SELECT_ALL_COUNTER_QUERY);
+            while (resultSet1.next()) {
+                writer.println("<h1> This page was visited " + resultSet1.getInt("count") + " times</h1>");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Group of students</h2>
+</body>
+</html>
